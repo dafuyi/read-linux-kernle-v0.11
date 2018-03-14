57fd5c73199e4eb8928f27407eecf0759eb4afcf
dafuyi <1427019390@qq.com>
2018-03-14 22:35:44 +0800

    NOTE to include/asm/io.h -> inb()

--- include/asm/io.h
+++ include/asm/io.h
@@ -1,7 +1,12 @@
 #define outb(value,port) \
 __asm__ ("outb %%al,%%dx"::"a" (value),"d" (port))
 
-
+/**
+ * 读取 I/O 端口 port 的宏定义,最后的变量 _v 的值
+ * 就是 inb() 的返回值.
+ *
+ *
+ */
 #define inb(port) ({ \
 unsigned char _v; \
 __asm__ volatile ("inb %%dx,%%al":"=a" (_v):"d" (port)); \
